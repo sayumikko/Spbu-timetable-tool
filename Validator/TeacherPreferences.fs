b5694ef59c52cecd3188c5a840d119d056b6918e
sayumikko <kuksant2708@mail.ru>
2023-12-22 14:03:58 +0300

    Added pretty printer, modified teacher preferences structure

--- Validator/TeacherPreferences.fs
+++ Validator/TeacherPreferences.fs
@@ -23,46 +23,84 @@ type AudienceEquipment =
     | Computer
     | TimeService // "Служба времени", запрос кафедры астрономии/астрофизики
     | Capacity of int
+    | Blackboard
+
+type Department =
+    | Astromony
+    | Astrophysics
+    | Algebra
+    | Geometry
+    | ComputationalMathematics
+    | DifferentialEquations
+    | Informatics
+    | InformationAndAnalyticalSystems
+    | Hydroaeromechanics
+    | OperationsResearch
+    | MathematicalAnalysis
+    | MathematicalPhysics
+    | CelestialMechanics
+    | ParallelAlgorithms
+    | AppliedCybernetics
+    | SystemProgramming
+    | StatisticalModeling
+    | TheoreticalAndAppliedMechanics
+    | TheoreticalCybernetics
+    | ProbabilityheoryAndMathematicalStatistics
+    | PhysicalMechanics
+    | ForeignLanguages
+    | TheoryOfElasticity
+
+type ClassType = 
+    | Lecture
+    | Laboratory
+    | Seminar
 
 type AudienceNumber = int
-type Time = int * int // Часы и минуты
-type StartTime = Time
-type FinalTime = Time
+type HoursAndMinutes = int * int // Часы и минуты
+type StartTime = HoursAndMinutes
+type FinalTime = HoursAndMinutes
 
 type Audience = AudienceNumber * Prioirity
 type TimeSlot = StartTime * FinalTime * Prioirity
-type DayOfWeekSlot = (DayOfWeek * Prioirity) option * TimeSlot list option
+type DayOfWeekSlot = DayOfWeek * Prioirity
+type Time = DayOfWeekSlot option * TimeSlot list option
 
-type GeneralPreferences =
-    | MaxDaysPerWeek of int
-    | MinDaysPerWeek of int
-    | MaxClassesPerDay of int
-    | MinClassesPerDay of int
-    | Gaps of (IsNeeded * Prioirity)
-
-and IsNeeded =
-    | IsNeeded
-    | Neutral
-    | Avoidable
+type PreferenceTarget =
+    | BySubject of string * ClassType option
+    | ByGroup of string * ClassType option
 
 type SpecificPreference =
-    { Target: PreferenceTarget
-      DesiredOptions: DesiredOption list }
-
-and PreferenceTarget =
-    | BySubject of string
-    | ByGroup of string
-
-and DesiredOption =
     | Audience of Audience
     | Equipment of AudienceEquipment
-    | Time of DayOfWeekSlot
+    | Time of Time
+    | UniteGroups of Prioirity
+    | UniteClasses of Prioirity
+    | AlternateBySubgroups of Prioirity
+    | UniteSubgroups of Prioirity
+    | OneClassInTwoWeeks of Prioirity
+    | InOneDay of Prioirity
 
 type TeacherPreference =
     | GeneralPreference of GeneralPreferences
-    | SpecificOptions of DesiredOption list
-    | SubjectOrGroupSpecificPreference of SpecificPreference
+    | SpecificPreference of SpecificPreference
+    | SubjectOrGroupSpecificPreference of PreferenceTarget * SpecificPreference list
 
-type Teacher =
-    { Name: string
+and GeneralPreferences =
+    | MaxDaysPerWeek of int * Prioirity
+    | MinDaysPerWeek of int * Prioirity
+    | FreeDaysPerWeek of int * Prioirity
+    | MaxClassesPerDay of int * Prioirity
+    | MinClassesPerDay of int * Prioirity
+    | Compactness of Prioirity
+    | IntersectTimeWithTeacher of Teacher * Prioirity
+    | Gaps of Prioirity
+
+and Teacher =
+    { Name: TeacherName
+      Department: Department
       Preferences: TeacherPreference list }
+
+and TeacherName =
+    { Name: string
+      Surname: string
+      Patronymic: string }
