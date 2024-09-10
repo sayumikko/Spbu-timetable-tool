92bd6e8993cf438566a23d293902bb4d01d2a6e5
Kseniia Kuzmina <kuksant2708@mail.ru>
2024-09-10 17:11:05 +0300

    Fixed more linter's comments

--- ExcelTimetableParser/TimetableParser.fs
+++ ExcelTimetableParser/TimetableParser.fs
@@ -9,49 +9,63 @@ open ClosedXML.Excel
 open Timetable
 
 type ParsingResult =
-    {
-        Timetable: Timetable
-        Errors: string list
-    }
+    { Timetable: Timetable
+      Errors: string list }
 
 module ExcelParser =
     let enumeratorLength enumerator =
-        let rec inner initial (enumerator : IEnumerator) =
+        let rec inner initial (enumerator: IEnumerator) =
             match enumerator.MoveNext() with
             | true -> inner (initial + 1) enumerator
             | false -> initial
+
         inner 0 enumerator
 
-    let rec fromIEnumerator<'T> (enumerator : IEnumerator) =
-        seq { if enumerator.MoveNext() then enumerator.Current :?> 'T; yield! fromIEnumerator enumerator }
-         |> Seq.toList
+    let rec fromIEnumerator<'T> (enumerator: IEnumerator) =
+        seq {
+            if enumerator.MoveNext() then
+                enumerator.Current :?> 'T
+                yield! fromIEnumerator enumerator
+        }
+        |> Seq.toList
 
     let (|Group|_|) (str: string) =
-       let success, result = Int32.TryParse(str)
-       if success then Some(result)
-       else None
+        let success, result = Int32.TryParse(str)
+        if success then Some(result) else None
 
-    let listOfMatches (matches : MatchCollection) =
-        matches.GetEnumerator() |> fromIEnumerator<Match> |> Seq.map string |> Seq.toList
+    let listOfMatches (matches: MatchCollection) =
+        matches.GetEnumerator()
+        |> fromIEnumerator<Match>
+        |> Seq.map string
+        |> Seq.toList
 
-    let (|Date|_|) (str: string) = if str.ToLower().Trim() = "день" then Some () else None
-    let (|Time|_|) (str: string) = if str.ToLower().Trim() = "время" then Some () else None
+    let (|Date|_|) (str: string) =
+        if str.ToLower().Trim() = "день" then
+            Some()
+        else
+            None
 
-    let cellsToSeq (cells : IXLCells) =
+    let (|Time|_|) (str: string) =
+        if str.ToLower().Trim() = "время" then
+            Some()
+        else
+            None
+
+    let cellsToSeq (cells: IXLCells) =
         cells.GetEnumerator() |> fromIEnumerator<IXLCell>
 
     // Считаем ячейку значимой ттт она имеет текст и ширина/высота > 5
-    let cellIsMeaningful (cell : IXLCell) =
+    let cellIsMeaningful (cell: IXLCell) =
         let height = cell.WorksheetRow().Height
         let width = cell.WorksheetColumn().Width
         let value = cell.Value.ToString()
         value <> "" && height > 3 && width > 3
 
 
-    let straightenString (str : string) =
+    let straightenString (str: string) =
         let str = str.ReplaceLineEndings(" ")
         let splitted = str.Split()
-        let splitted = Seq.filter (fun (x : string) -> x.Length > 0) splitted
+        let splitted = Seq.filter (fun (x: string) -> x.Length > 0) splitted
         let str = String.Join(" ", splitted)
         str
 
@@ -59,21 +73,30 @@ module ExcelParser =
     // Пусть 2 - минимальное число ячеек с текстом для заголовка, мб его в константу
     let rec parseHeader startRow (worksheet: IXLWorksheet) =
         let potentialHeaderRow = worksheet.Row startRow
-        let usedCells = potentialHeaderRow.CellsUsed() |> cellsToSeq |> Seq.filter cellIsMeaningful
-        if Seq.length usedCells < 2 then parseHeader (startRow + 1) worksheet
-        else (startRow, potentialHeaderRow)
+
+        let usedCells =
+            potentialHeaderRow.CellsUsed()
+            |> cellsToSeq
+            |> Seq.filter cellIsMeaningful
+
+        if Seq.length usedCells < 2 then
+            parseHeader (startRow + 1) worksheet
+        else
+            (startRow, potentialHeaderRow)
 
 
     // Находим лекторов Фамилия И.О. или Фамилия ИО
-    let tryFetchLecturer (str : string) =
-        let pattern = @"([А-Я][а-я]* [А-Я]\.[А-Я]\.)|([А-Я][а-я]* [А-Я]\.[А-Я])|([А-Я][а-я]* [А-Я][А-Я]\ )";
+    let tryFetchLecturer (str: string) =
+        let pattern =
+            @"([А-Я][а-я]* [А-Я]\.[А-Я]\.)|([А-Я][а-я]* [А-Я]\.[А-Я])|([А-Я][а-я]* [А-Я][А-Я]\ )"
+
         let matches = Regex.Matches(str, pattern, RegexOptions.IgnoreCase)
         matches
 
     // Аудитория это 4-значное число
     // TODO: Скорректировать
-    let tryFetchAuditorium (str : string) =
-        let pattern = @"([0-9][0-9][0-9][0-9])|([0-9][0-9][0-9])|(0[0-9])";
+    let tryFetchAuditorium (str: string) =
+        let pattern = @"([0-9][0-9][0-9][0-9])|([0-9][0-9][0-9])|(0[0-9])"
         let matches = Regex.Matches(str, pattern, RegexOptions.IgnoreCase)
         matches
 
@@ -81,9 +104,14 @@ module ExcelParser =
     // TODO: Удалить все что в скобках мб
     let tryFetchTitle = id
 
-    let weekdayOfString (s : string) (errors: ResizeArray<string>) : DayOfWeek =
-        let alphas = Regex("[а-я]*");
-        let processed = alphas.Matches(s.ToLower()) |> listOfMatches |> List.head;
+    let weekdayOfString (s: string) (errors: ResizeArray<string>) : DayOfWeek =
+        let alphas = Regex("[а-я]*")
+
+        let processed =
+            alphas.Matches(s.ToLower())
+            |> listOfMatches
+            |> List.head
+
         match processed.ToLower() with
         | "понедельник" -> Monday
         | "вторник" -> Tuesday
@@ -91,28 +119,57 @@ module ExcelParser =
         | "четверг" -> Thursday
         | "пятница" -> Friday
         | "суббота" -> Saturday
-        | _ ->  errors.Add($"Не удалось распознать {s} как день недели. Было вставлено воскресенье")
-                Sunday
+        | _ ->
+            errors.Add($"Не удалось распознать {s} как день недели. Было вставлено воскресенье")
+            Sunday
 
-    let convertStringToTime (weekDay : string) (str : string) (cell : IXLCell) (errors: ResizeArray<string>) =
+    let convertStringToTime (weekDay: string) (str: string) (cell: IXLCell) (errors: ResizeArray<string>) =
         // Converts xx:yy to xx, yy
-        let convTime (str : string) =
-            let [|hh; mm|] = str.Split(":")
+        let convTime (str: string) =
+            let [| hh; mm |] = str.Split(":")
             (Int32.Parse(hh), Int32.Parse(mm))
 
         let weekDay = weekdayOfString weekDay errors
-        let timePattern = @"[0-9][0-9]:[0-9][0-9]";
+        let timePattern = @"[0-9][0-9]:[0-9][0-9]"
         let matches = Regex.Matches(str, timePattern, RegexOptions.IgnoreCase)
+
         match matches |> listOfMatches with
-        | [s; e] -> Some {Weekday = weekDay; StartTime = (convTime s); EndTime = (convTime e)}
-        | [s] -> Some {Weekday = weekDay; StartTime = (convTime s); EndTime = (convTime s)}
-        | _ -> errors.Add($"Не выходит распознать время из ячейки {cell.Address.ToString()} {str}"); None
+        | [ s; e ] ->
+            Some
+                { Weekday = weekDay
+                  StartTime = (convTime s)
+                  EndTime = (convTime e) }
+        | [ s ] ->
+            Some
+                { Weekday = weekDay
+                  StartTime = (convTime s)
+                  EndTime = (convTime s) }
+        | _ ->
+            errors.Add($"Не выходит распознать время из ячейки {cell.Address.ToString()} {str}")
+            None
 
 
-    let fetchActivity (str : string) =
+    let fetchActivity (str: string) =
         let mutable result = NoClassType
-        if Regex.Matches(str, @"(\(пр\. з\.\))|(прак)", RegexOptions.IgnoreCase).Count > 0 then result <- Practical
-        if Regex.Matches(str, @"\(лекц\.\)|(лекц)", RegexOptions.IgnoreCase).Count > 0 then result <- Lecture
+
+        if Regex
+            .Matches(
+                str,
+                @"(\(пр\. з\.\))|(прак)",
+                RegexOptions.IgnoreCase
+            )
+            .Count > 0 then
+            result <- Practical
+
+        if Regex
+            .Matches(
+                str,
+                @"\(лекц\.\)|(лекц)",
+                RegexOptions.IgnoreCase
+            )
+            .Count > 0 then
+            result <- Lecture
+
         result
 
     let someOr none option =
@@ -122,29 +179,49 @@ module ExcelParser =
 
 
     // Обрабатываем одну запись о лекции
-    let processLecture (cell : IXLCell) (str : string) (weekDay : string) (timeString : string) (errors: ResizeArray<string>) : TimetableSlot option =
+    let processLecture
+        (cell: IXLCell)
+        (str: string)
+        (weekDay: string)
+        (timeString: string)
+        (errors: ResizeArray<string>)
+        : TimetableSlot option =
         let teachers = tryFetchLecturer str
-        if teachers.Count = 0 then errors.Add($"Преподаватель не найден для {str}")
+
+        if teachers.Count = 0 then
+            errors.Add($"Преподаватель не найден для {str}")
+
         let teachers = teachers |> listOfMatches
 
         let auditoriums = tryFetchAuditorium str
-        if auditoriums.Count = 0 then errors.Add($"Аудитория не найдена для {str}")
-        let auditoriums = auditoriums |> listOfMatches |> Seq.map Int32.Parse |> Seq.distinct
+
+        if auditoriums.Count = 0 then
+            errors.Add($"Аудитория не найдена для {str}")
+
+        let auditoriums =
+            auditoriums
+            |> listOfMatches
+            |> Seq.map Int32.Parse
+            |> Seq.distinct
 
         let auditorium = auditoriums |> Seq.tryHead |> someOr 0
-        if auditoriums |> Seq.length > 1 then errors.Add($"Было найдено несколько аудиторий %A{auditoriums}. Была выбрана первая: {auditorium}")
+
+        if auditoriums |> Seq.length > 1 then
+            errors.Add($"Было найдено несколько аудиторий %A{auditoriums}. Была выбрана первая: {auditorium}")
 
         let lectureTitle = tryFetchTitle str
 
         match convertStringToTime weekDay timeString cell errors with
         | Some time ->
             let activity = fetchActivity str
-            Some { StartTime = time.StartTime
-                   EndTime = time.EndTime
-                   Subject = lectureTitle
-                   Teachers = teachers
-                   Audience = auditorium
-                   ClassType = activity }
+
+            Some
+                { StartTime = time.StartTime
+                  EndTime = time.EndTime
+                  Subject = lectureTitle
+                  Teachers = teachers
+                  Audience = auditorium
+                  ClassType = activity }
         | None ->
             errors.Add($"Ошибка обработки времени для ячейки {cell.Address.ToString()}")
             None
@@ -152,48 +229,69 @@ module ExcelParser =
     let mutable weekdayString = ""
     let mutable timeString = ""
 
-    let processRow (tt : Timetable) (headerRow : IXLRow) (row : IXLRow) (errors: ResizeArray<string>) =
+    let processRow (tt: Timetable) (headerRow: IXLRow) (row: IXLRow) (errors: ResizeArray<string>) =
         let worksheet = row.Worksheet
 
         // Большая ячейка -> все маленькие ячейки с ее содержанием
-        let flattenSubCells (cells : IXLCell seq) =
-            let subCells (cell : IXLCell) =
+        let flattenSubCells (cells: IXLCell seq) =
+            let subCells (cell: IXLCell) =
                 if cell.IsMerged() then
                     let subCells = cell.MergedRange().Unmerge().Cells() |> cellsToSeq
-                    let updated = subCells |> Seq.map (fun cell -> cell.SetValue(cell.Value))
+
+                    let updated =
+                        subCells
+                        |> Seq.map (fun cell -> cell.SetValue(cell.Value))
+
                     updated
-                else [cell]
+                else
+                    [ cell ]
+
             Seq.map subCells cells |> Seq.concat
 
         // Если это группа, добавляем
-        let processCell (cell : IXLCell) =
+        let processCell (cell: IXLCell) =
             assert (cell.AsRange().ColumnCount() = 1)
             let cellValue = cell.Value.ToString()
             let headerRange = cell.WorksheetColumn().Intersection(headerRow)
             assert (headerRange.NumberOfCells > 0)
-            let headerValue = worksheet.Cell(headerRange.FirstAddress) |> (fun cell -> cell.Value.ToString())
+
+            let headerValue =
+                worksheet.Cell(headerRange.FirstAddress)
+                |> (fun cell -> cell.Value.ToString())
+
             match headerValue, cellValue with
             | Date, nonempty when nonempty <> "" -> weekdayString <- cellValue
             | Time, _ -> timeString <- cellValue
             | Group _, "" -> ()
             | Group g, _ ->
                 let straightened = straightenString cellValue
+
                 match processLecture cell straightened weekdayString timeString errors with
                 | Some lectureEntry ->
                     match convertStringToTime weekdayString timeString cell errors with
                     | Some time ->
-                        tt.TryAdd((time, g.ToString()), lectureEntry) |> ignore
+                        tt.TryAdd((time, g.ToString()), lectureEntry)
+                        |> ignore
                     | None -> errors.Add($"Ошибка обработки времени для ячейки {cell.Address.ToString()}")
                 | None -> errors.Add($"Ошибка обработки лекции для ячейки {cell.Address.ToString()}")
-            | _ -> errors.Add($"Ячейка {headerRange.FirstAddress.ToString()} со значением {headerValue} не является группой или датой/временем")
+            | _ ->
+                errors.Add(
+                    $"Ячейка {headerRange.FirstAddress.ToString()} со значением {headerValue} не является группой или датой/временем"
+                )
 
 
 
-        let cellSeq = row.Cells() |> cellsToSeq |> Seq.filter cellIsMeaningful |> flattenSubCells |> Seq.toList
+        let cellSeq =
+            row.Cells()
+            |> cellsToSeq
+            |> Seq.filter cellIsMeaningful
+            |> flattenSubCells
+            |> Seq.toList
+
         Seq.iter processCell cellSeq
 
-    let processWorksheet (worksheet: IXLWorksheet) (errors: ResizeArray<string>) : Timetable * string list = 
-        let tt = ttEmpty()
+    let processWorksheet (worksheet: IXLWorksheet) (errors: ResizeArray<string>) : Timetable * string list =
+        let tt = ttEmpty ()
 
         // Ищем заголовок: Первая строка
         // Заголовок обязан содержать более двух ячеек с ненулевым содержанием
@@ -210,9 +308,9 @@ module ExcelParser =
         (tt, List.ofSeq errors)
 
 
-    let processWorksheets (workbook: IXLWorkbook) (errors: ResizeArray<string>) : Timetable * string list = 
-        let finalTimetable = ttEmpty()
-    
+    let processWorksheets (workbook: IXLWorkbook) (errors: ResizeArray<string>) : Timetable * string list =
+        let finalTimetable = ttEmpty ()
+
         let expectedSheetNames =
             [ "1 курс"
               "2 курс"
@@ -220,7 +318,7 @@ module ExcelParser =
               "4 курс"
               "5 курс"
               "6 курс" ]
-    
+
         for sheet in workbook.Worksheets do
             let isValidSheetName =
                 expectedSheetNames
@@ -228,8 +326,10 @@ module ExcelParser =
 
             if isValidSheetName then
                 let timetable, sheetErrors = processWorksheet sheet errors
-                for KeyValue(key, value) in timetable do
+
+                for KeyValue (key, value) in timetable do
                     finalTimetable.[key] <- value
+
                 errors.AddRange(sheetErrors)
             else
                 errors.Add(sprintf "Найден лист с неожиданным названием '%s'. Он будет пропущен" sheet.Name)
@@ -238,10 +338,12 @@ module ExcelParser =
 
     let processFile (filePath: string) : Result<ParsingResult, string> =
         try
-            use workbook = new XLWorkbook(filePath)           
+            use workbook = new XLWorkbook(filePath)
             let errors = ResizeArray<string>()
             let timetable, parsingErrors = processWorksheets workbook errors
 
-            Ok {Timetable = timetable; Errors = List.ofSeq parsingErrors}
+            Ok
+                { Timetable = timetable
+                  Errors = List.ofSeq parsingErrors }
         with
         | :? Exception as ex -> Error ex.Message
